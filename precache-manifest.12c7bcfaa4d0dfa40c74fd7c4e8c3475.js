0b015af8f32f147e8a3fd08163a80b20f40f149b
Felix Ordonez <flordonez1@gmail.com>
2020-10-19 02:36:02 -0500

    Updates

---
+++ precache-manifest.12c7bcfaa4d0dfa40c74fd7c4e8c3475.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a39b81ffa2def42b9bf838f3d0e9ed59",
+    "url": "/portfolio_website_reactjsSPA/index.html"
+  },
+  {
+    "revision": "455d61db917f39da962d",
+    "url": "/portfolio_website_reactjsSPA/static/css/main.35838564.chunk.css"
+  },
+  {
+    "revision": "f7a1422fe5a9c5dc508c",
+    "url": "/portfolio_website_reactjsSPA/static/js/2.6362b6df.chunk.js"
+  },
+  {
+    "revision": "a09bcd6464a11c77a5caeae57fde5da8",
+    "url": "/portfolio_website_reactjsSPA/static/js/2.6362b6df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "455d61db917f39da962d",
+    "url": "/portfolio_website_reactjsSPA/static/js/main.8a3a2169.chunk.js"
+  },
+  {
+    "revision": "54806252a7ee503e1322",
+    "url": "/portfolio_website_reactjsSPA/static/js/runtime-main.7a71f8cc.js"
+  },
+  {
+    "revision": "5e10880ecdcf8c5379bb702b65e9a7e8",
+    "url": "/portfolio_website_reactjsSPA/static/media/felix2.5e10880e.png"
+  }
+]);
